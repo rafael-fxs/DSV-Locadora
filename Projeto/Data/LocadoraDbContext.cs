0643d3516c89bd9e84c37d60cf0efae490da6477
Rafael Felipe <rafa_el_felipe@hotmail.com>
2023-10-05 22:14:38 -0300

    Adicionado Projeto Bruno

--- Projeto/Data/LocadoraDbContext.cs
+++ Projeto/Data/LocadoraDbContext.cs
@@ -12,6 +12,8 @@ public class LocadoraDbContext : DbContext
     public DbSet<Jogos>? Jogos { get; set; }
     public DbSet<Pagamento> Pagamentos { get; set; }
     public DbSet<Locacao> Locacoes { get; set; }
+    public DbSet<Cliente> Cliente { get; set; }
+    public DbSet<Funcionario> Funcionario { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
